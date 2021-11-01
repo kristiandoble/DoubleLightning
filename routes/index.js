0b578dc0df01ab11fd3ee80eedcd91673412b02c
Kris D <doble.kristian@gmail.com>
2021-11-01 16:53:46 +0100

    Eight Commit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Lightning' , message: "Welcome to Lightning Payment"});
+  res.render('index', { title: 'Lightning' });
 });
 
 router.get('/index.html', function(req, res, next) {
